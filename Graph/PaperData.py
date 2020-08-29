0a97514fc1bafaf7c6b1e0c31f53d304738b3b74
adamparslow <adam.parslow@hotmail.com>
2020-08-29 12:03:16 +1000

    Got positioning to work

---
+++ Graph/PaperData.py
@@ -0,0 +1,7 @@
+class PaperData: 
+    def __init__(self, author, title, year, id, citations):
+        self.author = author
+        self.title = title
+        self.year = year
+        self.id = id
+        self.citations = citations
