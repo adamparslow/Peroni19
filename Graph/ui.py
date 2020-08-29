4907d6cc88222a0fa59a8227bf5d361cf716a486
Adam T <ataras2@gmail.com>
2020-08-29 22:35:48 +1000

    one more data

--- Graph/ui.py
+++ Graph/ui.py
@@ -222,7 +222,7 @@ papers.append(newPaper27)
 newPaper28 = PaperData("Tzu-Hsuan Pan", "Short and robust silicon mode (de)multiplexers using shortcuts to adiabaticity", "2015", "/home/aztar/Dropbox/SyncHacc/Peroni19/Papers/oe-23-8-10405.pdf", 28, [], "", 0)
 papers.append(newPaper28)
 
-newPaper29 = PaperData("Guery-Odelin", "Shortcuts to adiabaticity: Concepts, methods, and applications", "2019", "/home/aztar/Dropbox/SyncHacc/Peroni19/PapersWithTwoCol/4 - RevModPhys.91.045001.pdf", 29, [], "", 0)
+newPaper29 = PaperData("F. Impens", "Fast quantum control in dissipative systems using dissipationless solutions", "2019", "/home/aztar/Dropbox/SyncHacc/Peroni19/PapersWithTwoCol/Fast_quantum_control_in_dissip.pdf", 29, [], "", 0)
 papers.append(newPaper29)
 
 newPaper30 = PaperData("Xiao-Jing Lu", "Fast and robust population transfer in two-level quantum systems with dephasing noise and/or systematic frequency errors", "2013", "/home/aztar/Dropbox/SyncHacc/Peroni19/PapersWithTwoCol/PhysRevA.88.033406.pdf", 30, [], "", 0)
